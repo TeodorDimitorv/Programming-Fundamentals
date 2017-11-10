e6421a7ce82af5d510f1df892dc1b66ad9e6e9cc
TeodorDimitorv <teodor.y.dimitrov@gmail.com>
2017-11-10 10:55:05 +0200

    Add files via upload

---
+++ 1. Distinct List/DistinctList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DistinctList
+{
+	class DistinctList
+	{
+		static void Main()
+
+		{
+			List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+			for (int i = 0; i < list.Count; i++)
+			{
+				for (int j = 0; j < list.Count; j++)
+				{
+					if (list[i] == list[j])
+					{
+						list.RemoveAt(i);
+					}
+				}
+			}
+			Console.WriteLine(string.Join(" ", list));
+			//==========================================================
+
+
+		}
+	}
+}
